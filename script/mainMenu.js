f8ebb6e25fa77d8fb70bb4e13d15b33f9051f94f
Felipe Costa <felipe95176@gmail.com>
2024-04-27 10:47:14 -0300

    Criei uma borda para os rects do menu inicial, ela tem um blur para dar um charme.

--- script/mainMenu.js
+++ script/mainMenu.js
@@ -30,43 +30,70 @@ function charAndSpaceship()
     const nave_menu = document.querySelector('.nave_menu');
     nave_menu.style.display = 'block';
 }
-class Cloud {
-    constructor(left, right, animation, animationDuration) {
-        this.cloudElement = document.createElement('div');
-        this.cloudElement.classList.add('cloudMain');
-        this.cloudElement.style.height = Math.floor(Math.random() * 4) + 'em';
-        this.cloudElement.style.top = Math.floor(Math.random() * (window.innerHeight)) + 'px';
-        this.cloudElement.style.width = Math.floor(Math.random() * 10) + 'em';
-        this.cloudElement.style.left = left;
-        this.cloudElement.style.right = right;
-        this.cloudElement.style.animation = animation;
-        this.cloudElement.style.animationDuration = animationDuration;
+class Clouds{
+    constructor(topContainer, leftContainer, animation, animationDuration, 
+        top1, left1, height1, width1, 
+        top2, left2, height2, width2, 
+        top3, left3, height3, width3){
+        this.containerClouds = document.createElement('div');
+        this.containerClouds.classList.add('containerClouds');
+        this.containerClouds.style.top = topContainer;
+        this.containerClouds.style.left = leftContainer;
+        this.containerClouds.style.animation = animation;
+        this.containerClouds.style.animationDuration = animationDuration;
+
+        this.cloud1 = document.createElement('div');
+        this.cloud1.classList.add('clouds');
+        this.cloud1.style.top = top1;
+        this.cloud1.style.left = left1;
+        this.cloud1.style.height = height1;
+        this.cloud1.style.width = width1;
+        this.containerClouds.appendChild(this.cloud1);
+
+        this.cloud2 = document.createElement('div');
+        this.cloud2.classList.add('clouds');
+        this.cloud2.style.top = top2;
+        this.cloud2.style.left = left2;
+        this.cloud2.style.height = height2;
+        this.cloud2.style.width = width2;
+        this.containerClouds.appendChild(this.cloud2);
+
+        this.cloud3 = document.createElement('div');
+        this.cloud3.classList.add('clouds');
+        this.cloud3.style.top = top3;
+        this.cloud3.style.left = left3;
+        this.cloud3.style.height = height3;
+        this.cloud3.style.width = width3;
+        this.containerClouds.appendChild(this.cloud3);
+        
     }
-    addToBody() {
-        document.body.appendChild(this.cloudElement);
-    }
+
 }
 function clouds() {
-    for (let i = 0; i < 8; i++) 
-    {
-        const cloudLeft = new Cloud(
-            Math.floor(Math.random() * -500) + 'px',
-            500,
-            'moveLeftToRight 100s linear infinite',
-            (Math.random() * ((150 - 50)) + 50) + 's'
-        );
-        cloudLeft.addToBody();
-    }
-    for (let i = 0; i < 2; i++) 
-    {
-        const cloudRight = new Cloud(
-            500,
-            Math.floor(Math.random() * 500) + 'px',
-            'moveRightToLeft 5s linear infinite',
-            (Math.random() * ((250 - 150)) + 150) + 's'
-        );
-        cloudRight.addToBody();
-    }
+    for(i=0; i<5; i++){
+        const cloudsGenerator = new Clouds(
+        
+        Math.random()*75+'%',
+        Math.floor(Math.random() * (-25 - (-75)) + (-75))+'%',
+        'goLeft 5s linear infinite',
+        Math.floor(Math.random() * (100 - 50) + 50)+'s', 
+        Math.floor(Math.random() * (60 - 50) + 50) +'%',
+        Math.floor(Math.random() * (30 - 5) + 5) +'%',
+        Math.floor(Math.random() * (40 - 35) + 35) +'%',
+        Math.floor(Math.random() * (70 - 60) + 60) +'%',
+
+        Math.floor(Math.random() * (40 - 30) + 30) +'%',
+        Math.floor(Math.random() * (30 - 5) + 5) +'%',
+        Math.floor(Math.random() * (50 - 35) + 35) +'%',
+        Math.floor(Math.random() * (50 - 40) + 40) +'%',
+        
+        Math.floor(Math.random() * (20 - 5) + 5) +'%',
+        Math.floor(Math.random() * (50 - 5) + 5) +'%',
+        Math.floor(Math.random() * (50 - 35) + 35) +'%',
+        Math.floor(Math.random() * (40 - 20) + 20) +'%'
+    );
+    document.body.appendChild(cloudsGenerator.containerClouds);
+}
 }
 class Rect {
     constructor(top, left, rect_text, play, options) {
@@ -77,6 +104,22 @@ class Rect {
         this.rect.innerHTML = rect_text;
         play; options;
 
+        const rect_border = document.createElement('div');
+        rect_border.classList.add('rect_menu_border');
+        rect_border.style.top = top;
+        rect_border.style.left = left;
+        document.body.appendChild(rect_border);
+
+        this.rect.addEventListener('mouseenter', () => {
+            this.rect.style.animation = 'rainbow_border 5s ease-in-out infinite';
+            rect_border.style.animation = 'rainbow_border 5s ease-in-out infinite';
+            
+            });
+            this.rect.addEventListener('mouseleave', () => {
+            this.rect.style.animation = 'none';
+            rect_border.style.animation = 'none';
+              
+        });
         switch (true) {
             case play:
                 this.rect.addEventListener('click', () => {
@@ -94,6 +137,7 @@ class Rect {
     }
     addToBody() {
         document.body.appendChild(this.rect);
+        
     }
 }
 function rectsForMenu()
@@ -103,6 +147,8 @@ function rectsForMenu()
 
     const rect_options = new Rect(65 + '%', 50 + '%', 'Opções', false, true)
     rect_options.addToBody();
+
+    
 }
 function removeTela0()
 {
@@ -114,17 +160,25 @@ function removeTela0()
 
     const rects = document.querySelectorAll('.rect_menu');
     rects.forEach(rects => {
-        rects.style.display = 'none';
+        rects.remove();
     });
-
+    const rects_border = document.querySelectorAll('.rect_menu_border');
+    rects_border.forEach(rects_border => {
+        rects_border.remove();
+    });
     const character = document.querySelector('.character');
-    character.style.display = 'none';
+    character.remove();
     const nave_menu = document.querySelector('.nave_menu');
-    nave_menu.style.display = 'none';
+    nave_menu.remove();
 
     const stars = document.querySelectorAll('.stars');
     stars.forEach(stars => {
-        stars.style.display = 'none';
+        stars.remove();
+    });
+
+    const allClouds = document.querySelectorAll('.containerClouds');
+    allClouds.forEach(allClouds => {
+        allClouds.remove();
     });
 }
 function initialScreen() {
